62dec8d5642aa76eb8df8ea45d3f5d8cb165a7c5
Tom Campion <tomcampion10@laposte.net>
2019-12-20 13:20:29 +0100

    install flex, adapt file architecture

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('default/index.html.twig');
+    }
+}
