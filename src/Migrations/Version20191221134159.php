d8b1c5d170f0661b386f6e8fdf32be9ae87863b4
Tom Campion <tomcampion10@laposte.net>
2019-12-21 15:01:15 +0100

    attached the task to the current user when creating the task

---
+++ src/Migrations/Version20191221134159.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191221134159 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE task ADD user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE task ADD CONSTRAINT FK_527EDB25A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_527EDB25A76ED395 ON task (user_id)');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6498DB60186');
+        $this->addSql('DROP INDEX IDX_8D93D6498DB60186 ON user');
+        $this->addSql('ALTER TABLE user DROP task_id');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE task DROP FOREIGN KEY FK_527EDB25A76ED395');
+        $this->addSql('DROP INDEX IDX_527EDB25A76ED395 ON task');
+        $this->addSql('ALTER TABLE task DROP user_id');
+        $this->addSql('ALTER TABLE user ADD task_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6498DB60186 FOREIGN KEY (task_id) REFERENCES task (id)');
+        $this->addSql('CREATE INDEX IDX_8D93D6498DB60186 ON user (task_id)');
+    }
+}
